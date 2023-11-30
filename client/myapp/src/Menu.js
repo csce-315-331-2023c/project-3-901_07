57990d6c0b5173d4d835bc1c91afcc18d6cb528c
Quenton Hua <hua.quenton@gmail.com>
2023-11-29 20:27:20 -0600

    changes to Menu.js

--- client/myapp/src/Menu.js
+++ client/myapp/src/Menu.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './Menu.css';
 function Menu() {
+  const [drinkAndToppingData, setdrinkAndToppingData] = useState(false);
+  const webServerAddress = process.env.REACT_APP_WEB_SERVER_ADDRESS;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response_drink = await fetch(webServerAddress + "/menu_item", {
+          mode: "cors",
+        });
+        console.log(response_drink);
+        const drink_data = await response_drink.json();
 
+        const response_topping = await fetch(webServerAddress + "/topping", {
+          mode: "cors",
+        });
+        const topping_data = await response_topping.json();
+
+        const formattedData = {
+          menu_items: drink_data,
+          toppings: topping_data,
+        };
+        setdrinkAndToppingData(formattedData);
+      } catch {
+        //console.log("error");
+      }
+    }
+    fetchData();
+  }, [webServerAddress]);
 
 
   
@@ -9,6 +35,19 @@ function Menu() {
   return (
     <div>
         <p>Hello, World!</p>
+        {drinkAndToppingData &&
+        Object.entries(drinkAndToppingData.menu_items).map(([category, items]) => (
+          <div id={category} key={category}>
+            <h3>{category}</h3>
+            <div>
+              {items.map((item) => (
+                <div>
+                  {item.name}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
