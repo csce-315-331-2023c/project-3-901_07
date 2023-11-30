7f04051b8638cca8412c78491624166674785ce8
Quenton Hua <hua.quenton@gmail.com>
2023-11-29 22:53:24 -0600

    added unique images for some drinks, drinks now render their respective images

--- client/myapp/src/scenes/Home/components/drinkcard/index.js
+++ client/myapp/src/scenes/Home/components/drinkcard/index.js
@@ -1,10 +1,22 @@
 import "./styles.css";
-import defaultDrinkImage from "../../../../assets/images/defaultdrinkimage.png";
+import defaultDrinkImage from "../../../../assets/images/drinks/defaultdrinkimage.png";
 
 function DrinkCard({toggleModal,drinkProperties}) {
+    // console.log(`${drinkProperties.name.toLowerCase()}.png`);
+    function getDrinkImage() {
+            try {
+                const imagePath = require(`../../../../assets/images/drinks/${drinkProperties.name.toLowerCase()}.png`);
+                return imagePath;
+            } catch (err) {
+                return defaultDrinkImage;
+            }
+  
+    }
+
+
     return(
         <div className = "drinkcard" onClick={toggleModal}>
-            <img className="drinkcard-image" src={defaultDrinkImage} alt="Default Drink" />
+            <img className="drinkcard-image" src={getDrinkImage()} alt="Default Drink" />
             <div className = "drinkcard-name">
                 {drinkProperties.name}
             </div>
