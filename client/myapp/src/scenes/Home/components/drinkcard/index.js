7f493a62fbee0d1b0fc8f9cee87daffbcc177041
Quenton Hua <hua.quenton@gmail.com>
2023-11-30 15:09:14 -0600

    added rest of images for drinks and toppings

--- client/myapp/src/scenes/Home/components/drinkcard/index.js
+++ client/myapp/src/scenes/Home/components/drinkcard/index.js
@@ -1,32 +1,56 @@
 import "./styles.css";
 import defaultDrinkImage from "../../../../assets/images/drinks/defaultdrinkimage.png";
 
-function DrinkCard({toggleModal,drinkProperties}) {
-    // console.log(`${drinkProperties.name.toLowerCase()}.png`);
-    function getDrinkImage() {
-            try {
-                const imagePath = require(`../../../../assets/images/drinks/${drinkProperties.name.toLowerCase()}.png`);
-                return imagePath;
-            } catch (err) {
-                return defaultDrinkImage;
-            }
-  
+function DrinkCard({ toggleModal, drinkProperties, currView }) {
+  // console.log(`${drinkProperties.name.toLowerCase()}.png`);
+  function getDrinkImage() {
+    try {
+      const imagePath = require(`../../../../assets/images/drinks/${drinkProperties.name.toLowerCase()}.png`);
+      return imagePath;
+    } catch (err) {
+      return defaultDrinkImage;
     }
+  }
 
 
-    return(
-        <div className = "drinkcard" onClick={toggleModal}>
-            <img className="drinkcard-image" src={getDrinkImage()} alt="Default Drink" />
-            <div className = "drinkcard-name">
-                {drinkProperties.name}
-            </div>
-            <div className = "drinkcard-price">
-                ${drinkProperties.price}
-            </div>
+  let drinkcard = "drinkcard ";
+  let drinkcardcontent = "drinkcard-content ";
+  let drinkcardname = "drinkcard-name ";
+  let drinkcardprice = "drinkcard-price ";
+  
+  let viewSuffix = "";
+  switch (currView) {
+    case "customer":
+      viewSuffix = "customer";
+      break;
+    case "cashier":
+    case "manager":
+      viewSuffix = "employee";
+      break;
+  }
+  
+  drinkcard += viewSuffix;
+  drinkcardcontent += viewSuffix;
+  drinkcardname += viewSuffix;
+  drinkcardprice += viewSuffix;
+  
+  console.log(drinkProperties)
 
-        </div>
-    );
-
+  return (
+    <div className={drinkcard} onClick={toggleModal}>
+      {currView === "customer" && ( // Render image only if currView is "customer"
+        <img
+          className="drinkcard-image"
+          src={getDrinkImage()}
+          alt="Default Drink"
+        />
+      )}
+      <div className={drinkcardcontent}>
+        <div className={drinkcardname}>{drinkProperties.name}</div>
+        <div className={drinkcardprice}>${drinkProperties.price}</div>
+      </div>
+    </div>
+  );
 }
 
 export default DrinkCard;
--- client/myapp/src/scenes/Home/components/toppingcard/index.js
+++ client/myapp/src/scenes/Home/components/toppingcard/index.js
@@ -2,7 +2,7 @@ import "./styles.css";
 import React, { useState, useEffect } from "react";
 import defaultToppingImage from "../../../../assets/images/boba.png";
 
-function ToppingCard({ topping, toppingClick, isActiveEdit }) {
+function ToppingCard({ topping, toppingClick, isActiveEdit, currView }) {
   const [isActive, setIsActive] = useState(false);
   const toggleActiveClass = () => {
     console.log("toggleActiveClass called");
@@ -11,36 +11,53 @@ function ToppingCard({ topping, toppingClick, isActiveEdit }) {
 
   function getToppingImage() {
     try {
-        const imagePath = require(`../../../../assets/images/toppings/${topping.name.toLowerCase()}.png`);
-        return imagePath;
+      const imagePath = require(`../../../../assets/images/toppings/${topping.name.toLowerCase()}.png`);
+      return imagePath;
     } catch (err) {
-        return defaultToppingImage;
-    }
-
-}
-
-useEffect(() => {
-  function checkIfToppingCardIsActive(){
-    if(isActiveEdit){
-      setIsActive(true);
+      return defaultToppingImage;
     }
   }
-  checkIfToppingCardIsActive();
-}, [isActiveEdit]);
+
+  useEffect(() => {
+    function checkIfToppingCardIsActive() {
+      if (isActiveEdit) {
+        setIsActive(true);
+      }
+    }
+    checkIfToppingCardIsActive();
+  }, [isActiveEdit]);
+
+  let toppingcard = "toppingcard ";
+  
+  let viewSuffix = "";
+  switch (currView) {
+    case "customer":
+      viewSuffix = "customer";
+      break;
+    case "cashier":
+    case "manager":
+      viewSuffix = "employee";
+      break;
+  }
+  
+  toppingcard += viewSuffix;
+  
 
   return (
     <div
-      className={`toppingcard ${isActive ? "active" : ""}`}
+      className={`${toppingcard} ${isActive ? "active" : ""}`}
       onClick={() => {
         toppingClick();
         toggleActiveClass();
       }}
     >
-      <img
-        className="toppingcard-image"
-        src={getToppingImage()}
-        alt="Default Topping"
-      />
+      {currView === "customer" && ( // Render image only if currView is "customer"
+        <img
+          className="toppingcard-image"
+          src={getToppingImage()}
+          alt="Default Topping"
+        />
+      )}
       <div className="topingcard-text">
         {topping.name} <br /> ${topping.price}
       </div>
