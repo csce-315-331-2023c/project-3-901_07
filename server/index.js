be08204e51b04ea7c3c3a0349769ae0afe7037ff
Quy <vanquytrxm@gmail.com>
2023-12-04 10:03:09 -0600

    Update to render

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const { Pool } = require("pg");
 const dotenv = require("dotenv").config();
 const path = require("path");
 
 // Create express app
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 const session = require("express-session");
 
 let cors = require("cors");
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extendex: true }));
 
 // Create pool
 const pool = new Pool({
@@ -668,7 +668,7 @@ app.get("/", function (req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at https://test331project.onrender.com/}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 /*  PASSPORT SETUP  */
